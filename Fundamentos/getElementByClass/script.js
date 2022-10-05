079bb0f313e7e24b9a440fb1265f9713636c09e6
Felipe Gerber <felipegerber@outlook.com>
2022-10-05 13:22:20 -0300

    Finalização dos exercicios de getElementByTAG e getEelementsByClassName

---
+++ Fundamentos/getElementByClass/script.js
@@ -0,0 +1,3 @@
+console.log(document.getElementsByClassName("paragrafos")); //retorna os dois parágrafos no terminal
+console.log(document.getElementsByClassName("paragrafos")[1].innerHTML); //Retorna o conteúdo dentro do HTML 
+document.getElementsByClassName('paragrafos')[1].innerHTML = 'mudando tudo que tá dentro do paragrafo'
